0a4f708c8e63cdefa80090bfa81661c965d496e3
moazahmed22 <nabelnadel21@gmail.com>
2025-05-27 21:35:04 +0300

    add updateItem function and manage some styling

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-let foodName = document.getElementById("foodName");
-let foodPrice = document.getElementById("foodPrice");
-let foodCalories = document.getElementById("foodCalories");
-let foodCategory = document.getElementById("foodCategory");
-let foodImg = document.getElementById("foodImg");
-let myRow = document.getElementById("myRow");
+const foodName = document.getElementById("foodName");
+const foodPrice = document.getElementById("foodPrice");
+const foodCalories = document.getElementById("foodCalories");
+const foodCategory = document.getElementById("foodCategory");
+const foodImg = document.getElementById("foodImg");
+const submitBtn = document.getElementById("submitBtn");
+const updateBtn = document.getElementById("updateBtn");
+const myRow = document.getElementById("myRow");
 let cardContainer = ``;
+let globalIndex;
 
 // check on local storage for foodDataBase
 let foodDataBase =
@@ -26,7 +29,7 @@ let addItem = () => {
 
   clearInputs();
 
-  saveLocalStorage(foodDataBase);
+  saveLocalStorage();
 };
 
 // function to show stored items
@@ -48,7 +51,7 @@ function showItems() {
                   </div>
                   <div class="d-flex flex-column w-100 gap-1">
                     <p class="item-title fs-5 m-0">
-                      <span class="">${foodDataBase[index].objName}</span>
+                      <span class="text-capitalize">${foodDataBase[index].objName}</span>
                     </p>
                     <p class="item-info d-flex justify-content-around align-items-center px-2 m-0">
                       <span class="">${foodDataBase[index].objCal} calories</span>
@@ -59,11 +62,9 @@ function showItems() {
                       class="d-flex align-items-center justify-content-around"
                     >
                       <p class="price m-0 d-inline">${foodDataBase[index].objPrice}$</p>
-                      <div
-                        class="button-container d-flex justify-content-between align-items-end"
-                      >
-                        <button class="border-0 d-flex justify-content-center align-align-items-center" onclick="deleteItem()">
-                          <i class="fa-solid fa-pen-to-square primary me-2" onclick="updateItem(${index})"></i>
+                      <div class="button-container d-flex justify-content-between align-items-end">
+                        <button class="border-0 d-flex justify-content-center align-align-items-center" onclick="itemToInputs(${index})"">
+                          <i class="fa-solid fa-pen-to-square primary me-2"></i>
                         </button>
                         <button 
                           class="border-0 d-flex justify-content-center align-align-items-center" 
@@ -89,13 +90,32 @@ let clearInputs = () => {
 };
 
 // function to store food dataBase in the browser's local storage
-let saveLocalStorage = (data) => {
-  window.localStorage.setItem("foodDataBase", JSON.stringify(data));
+let saveLocalStorage = () => {
+  window.localStorage.setItem("foodDataBase", JSON.stringify(foodDataBase));
 };
 
-// function to update item
-let updateItem = (index) => {
-
+// updating item method
+// function to set the update item back to inputs
+let itemToInputs = (index) => {
+  foodName.value = foodDataBase[index].objName;
+  foodPrice.value = foodDataBase[index].objPrice;
+  foodCalories.value = foodDataBase[index].objCal;
+  foodCategory.value = foodDataBase[index].objCat;
+  globalIndex = index;
+  submitBtn.setAttribute("style", "display:none !important;");
+  updateBtn.setAttribute("style", "display:block !important;");
+};
+// function to update item based on new inputs
+let updateItem = () => {
+  foodDataBase[globalIndex].objName = foodName.value;
+  foodDataBase[globalIndex].objPrice = foodPrice.value;
+  foodDataBase[globalIndex].objCal = foodCalories.value;
+  foodDataBase[globalIndex].objCat = foodCategory.value;
+  submitBtn.setAttribute("style", "display:block !important;");
+  updateBtn.setAttribute("style", "display:none !important;");
+  clearInputs();
+  saveLocalStorage();
+  showItems();
 };
 
 // function to delete item
